1a935bb4783a8068462fd7525a9c1ff8afc3b472
xiong <1095939843@qq.com>
2018-08-23 21:24:15 +0800

    添加OrderCustom文件

---
+++ src/pojo/OrderCustom.java
@@ -0,0 +1,43 @@
+package pojo;
+
+import pojo.Orders;
+
+public class OrderCustom extends Orders {
+
+    private String username;
+    private String sex;
+    private String address;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderCustom{" +
+                "username=" + username + "\'" +
+                ", sex=" + sex + "\'" +
+                ", address=" + address + "\'" +
+                "}";
+    }
+}
